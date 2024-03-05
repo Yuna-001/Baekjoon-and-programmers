2e34bb6bad7709812140251532e26c2329215594
Yuna-001 <161466246+Yuna-001@users.noreply.github.com>
2024-03-06 03:19:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,5 +1,3 @@
 function solution(my_string) {
-    const numArr = my_string.match(/\d+/g);
-    if (numArr === null) return 0;
-    return numArr.reduce((total,cur)=>total + +cur,0);
+    return my_string.split(/\D+/).reduce((acc,cur) => acc + +cur,0) 
 }
