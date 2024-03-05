474c7ce065a2b3a2fc6ec9d318e0fa0293db7f85
Yuna-001 <161466246+Yuna-001@users.noreply.github.com>
2024-03-06 03:00:50 +0900

    [level 0] Title: 다항식 더하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,29 @@
+function solution(polynomial) {
+    let x1 = 0;
+    let x0 = 0;
+    let result = "";
+    const arr = polynomial.split(" + ");
+    for(let i=0; i<arr.length; i++){
+        if (arr[i].at(-1) === "x"){
+            x1 +=  arr[i].length === 1 ? 1 : +arr[i].slice(0,arr[i].length-1);
+        }else{
+            x0 += +arr[i];
+        }
+    }
+    
+    if(x1 !== 0){
+        if(x1 === 1){
+            result+= `x`;
+        }else{
+            result+= `${x1}x`;
+        }
+        
+        if (x0 !== 0){
+            result+=` + ${x0}`;
+        }
+    } else{      
+            result+=`${x0}`;
+    }
+    
+    return result;
+}
