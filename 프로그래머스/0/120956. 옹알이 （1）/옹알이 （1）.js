f81302b48412dbae1873ea7a63e1a52bf9c39cfc
Yuna-001 <161466246+Yuna-001@users.noreply.github.com>
2024-03-10 01:37:35 +0900

    [level 0] Title: 옹알이 (1), Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,3 +1,11 @@
 function solution(babbling) {
-    return babbling.filter((str)=>str.replace(/aya|ye|woo|ma/g,"")==="").length;
+    return babbling.filter(word => canSpeak(word)).length;
+}
+
+function canSpeak(word){
+    const arr = ["aya","ye","woo","ma"];
+    for (const str of arr){
+        if (word.split(str).length > 2) return false;
+    }
+    return word.replace(/aya|ye|woo|ma/g,"")==="" ;
 }
