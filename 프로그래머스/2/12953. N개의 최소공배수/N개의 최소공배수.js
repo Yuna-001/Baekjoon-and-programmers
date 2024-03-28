571709a9ca299d5c6af6872e48cde295d02a51b8
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-03-28 22:11:46 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,5 +1,17 @@
 function solution(arr) {
-    for(let i=Math.max(...arr); ;i++){
-        if(arr.every(n=>i%n===0)) return i;
+    function getGCD(a,b){
+        return a%b===0 ? b : getGCD(b,a%b);
     }
+    
+    function getLCM(a,b){
+        return a*b/getGCD(a,b);
+    }
+    
+    let lcm = arr[0];
+    
+    for(let i=1; i<arr.length; i++){
+        lcm = getLCM(lcm,arr[i]);
+    }
+    
+    return lcm;
 }
