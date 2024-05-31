88a5e0d8d98dcc86319e0b8da9751178d4d3a9bb
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-05-31 23:57:14 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 1.41 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,13 +1,14 @@
 function solution(files) {
-    const reg = /([^\d]+)(\d+)/
-    return files.sort((a,b)=>{
-        let [,headA,numberA] = a.match(reg);
-        let [,headB,numberB] = b.match(reg);
-        
-        headA = headA.toLowerCase();
-        headB = headB.toLowerCase();
-        
-        if(headA !== headB) return headA > headB ? 1 : -1;
-        return +numberA - +numberB
+    const reg = /([^\d]+)(\d+)/;
+    const objArr = files.map(file => {
+        let [, head, number] = file.match(reg);
+        head = head.toLowerCase();
+        number = +number;
+        return { file , head, number };
     });
+    
+    return objArr.sort((a,b)=>{
+        if(a.head !== b.head) return a.head > b.head ? 1 : -1;
+        return a.number - b.number;
+    }).map(x => x.file);
 }
