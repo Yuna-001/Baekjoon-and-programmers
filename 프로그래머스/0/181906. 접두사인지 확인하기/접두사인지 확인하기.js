765bb9034baba04a054ac04bd7139daa0098b049
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-07-10 14:40:53 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,4 +1,3 @@
 function solution(my_string, is_prefix) {
-    const reg = new RegExp(`^${is_prefix}`);
-    return my_string.match(reg) === null ? 0 : 1;
+    return +my_string.startsWith(is_prefix);
 }
