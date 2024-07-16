daf1ee34d38c98ab1ca71b45065ea4f5a1116dbb
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-07-16 23:46:50 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,3 +1,7 @@
 function solution(date1, date2) {    
-    return new Date(...date1) < new Date(...date2) ? 1 : 0;
+    for(let i=0; i<date1.length; i++){
+        if(date1[i] !== date2[i]) return date1[i] < date2[i] ? 1 : 0;
+    }
+    
+    return 0;
 }
