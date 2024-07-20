c7884f8195199b5a4bde80e7a2365174aa9a22d8
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-07-20 18:16:04 +0900

    [level 2] Title: 프로세스, Time: 0.44 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,17 +1,15 @@
 function solution(priorities, location) {
     let queue = priorities.map((priority,index)=>({priority,index}));
     let count = 0;
-    let current;
     
-    while(current !== location){
-        const max = Math.max(...queue.map(el => el.priority));
-        const i = queue.findIndex(({priority})=>priority===max);
-        const arr = queue.splice(0,i+1);
+    while(true){
+        const element = queue.shift();
         
-        current = arr.pop().index;
-        queue = [...queue, ...arr];
-        count++;
+        if(queue.some(v => v.priority > element.priority)){
+            queue.push(element);
+        } else{
+            count++;
+            if(element.index === location) return count;
+        }
     }   
-    
-    return count;
 }
