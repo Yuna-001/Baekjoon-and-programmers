6221c31950279e8ed94922ca1f8c89f3a6a21ed0
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-01 23:48:24 +0900

    [level 0] Title: 배열 만들기 6, Time: 70.15 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    const stk = [];
+    
+    for(let i=0; i<arr.length; i++){
+        if(stk.length && stk.at(-1)===arr[i]){
+            stk.pop();
+        }else{
+            stk.push(arr[i]);
+        }
+    }
+    
+    return stk.length ? stk : [-1];
+}
