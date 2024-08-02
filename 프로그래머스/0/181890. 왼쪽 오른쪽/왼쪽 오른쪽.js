9ca944da177d959d6558edc331ffb6ac5ab1e848
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-02 23:36:04 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,16 +1,8 @@
 function solution(str_list) {
-    let c, index;
-    
     for(let i=0; i<str_list.length; i++){
-        if(str_list[i] === "l" || str_list[i] === "r"){
-            c = str_list[i];
-            index = i;
-            break;
-        }
+         if(str_list[i]==="l") return str_list.slice(0,i);
+         if(str_list[i]==="r") return str_list.slice(i+1);
     }
-    
-    if(c==="l") return str_list.slice(0,index);
-    if(c==="r") return str_list.slice(index+1);
     
     return [];
 }
