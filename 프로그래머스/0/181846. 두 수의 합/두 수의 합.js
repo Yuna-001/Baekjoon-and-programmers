74d43eac5d3d9d851fc87c3f8643ff4f3e7b5e1d
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-09 00:11:05 +0900

    [level 0] Title: 두 수의 합, Time: 19.14 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,3 +1,46 @@
 function solution(a, b) {
-   return BigInt(a) + BigInt(b) + "";
+    const arrA = [...a];
+    const arrB = [...b];
+    let carry = 0;
+    let result = "";
+    
+    while(arrA.length && arrB.length){
+        const n = carry + +arrA.pop() + +arrB.pop();
+        
+        if(n >= 10){
+            carry = 1;
+            result = n - 10 + result;
+        }else{
+            carry = 0;
+            result = n + result;
+        }
+    }
+    
+    while(arrA.length){
+        const n = carry + +arrA.pop();
+        
+        if(n >= 10){
+            carry = 1;
+            result = n - 10 + result;
+        }else{
+            carry = 0;
+            result = n + result;
+        }
+    }
+    
+    while(arrB.length){
+        const n = carry + +arrB.pop();
+        
+        if(n >= 10){
+            carry = 1;
+            result = n - 10 + result;
+        }else{
+            carry = 0;
+            result = n + result;
+        }
+    }
+    
+    if(carry) result = carry + result;
+    
+    return result;
 }
