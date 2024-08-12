08e579d5bff2ef980b59b920e715bac99578fe93
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-13 00:38:01 +0900

    [level 3] Title: 네트워크, Time: 30.62 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,30 +1,22 @@
 function solution(n, computers) {
     const visited = {};
-    const connection = Array.from({length:n},()=>[]);
     let network = 0;
     
-    for(let i=0; i<n; i++){
-        for(let j=i+1; j<n; j++){
-            if(computers[i][j]){
-                connection[i].push(j);
-                connection[j].push(i);
-            }
-        }
-    }
-   
-    function DFS(start){
-        for(const computer of connection[start]){
-            if(!visited[computer]){
-                visited[computer] = true;
-                DFS(computer)
-                
+    function dfs(start){
+        if(visited[start]) return;
+        
+        visited[start] = true;
+        
+        for(let end=0; end<n; end++){
+            if(computers[start][end]){
+                dfs(end);
             }
         }
     }
     
     for(let i=0; i<n; i++){
         if(!visited[i]){
-            DFS(i);
+            dfs(i);
             network++;
         }
     }
