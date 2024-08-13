c06fedce93e554c9f99421e9b7954ca3f14fb5fe
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-14 00:49:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.28 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,23 @@
+function solution(maps) {
+    const goal = [maps.length-1, maps[0].length-1];
+    const queue = [[0,0,1]];
+    const directions = [[1,0],[0,1],[-1,0],[0,-1]];
+    
+    while(queue.length){
+        const [row,col,step] = queue.shift();
+        
+        if(row === goal[0] && col === goal[1]) return step;
+        
+        for(const direction of directions){
+            const nextRow = row + direction[0];
+            const nextCol = col + direction[1];
+            
+            if(nextRow >= 0 && nextRow <= goal[0] && nextCol >= 0 && nextCol <= goal[1] && maps[nextRow][nextCol]){
+                queue.push([nextRow,nextCol,step+1]);
+                maps[nextRow][nextCol] = 0;
+            }
+        }
+    }
+    
+    return -1;
+}
