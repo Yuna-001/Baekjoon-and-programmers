864a6dc85a347275ad69ad55efb849d2cce1ec1e
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-22 00:08:36 +0900

    [level 2] Title: 시소 짝꿍, Time: 67.80 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,15 +1,14 @@
 function solution(weights) {
     let result = 0;
-    const cases = new Map();
-    
-    weights.sort((a,b)=>a-b);
+    const map = new Map();
+  
+    weights.sort((a,b)=>b-a);
     
     for(const weight of weights){
-        result += (cases.get(weight) || 0);
-        
         for(const n of [1,2,1.5,4/3]){
-            cases.set(weight*n, (cases.get(weight*n) || 0) + 1);
+            if(map.get(n*weight)) result += map.get(n*weight);
         }
+        map.set(weight, (map.get(weight) || 0 ) + 1);
     }
     
     return result;
