fee4bcf94803c153dcc049fe8015a62389a6b94c
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-08-28 22:38:53 +0900

    [level 0] Title: 카운트 업, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,9 +1,3 @@
 function solution(start_num, end_num) {
-    const answer = [];
-    
-    for(let i=start_num; i<=end_num; i++){
-        answer.push(i);
-    }
-    
-    return answer;
+    return Array.from({length:end_num-start_num+1},(_,i)=>i+start_num);
 }
