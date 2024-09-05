85c3305ed0fb398cbe69875c6f8bab91e7160519
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-09-05 23:23:22 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,1 +1,3 @@
-const solution = (n) => n%2 ? (n+1)**2/4 : n*(n+1)*(n+2)/6;
+function solution(n) {
+    return new Array(Math.ceil(n/2)).fill(0).reduce((acc,cur,i)=> n%2 === 1 ? acc + 2*i+1 : acc+ Math.pow(2*i+2,2),0);
+}
