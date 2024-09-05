c8857df3f636620400d766d019f04b0fedeb7bf1
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-09-05 23:33:25 +0900

    [level 0] Title: 가까운 수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,16 +1,3 @@
 function solution(array, n) {
-    let result;
-    let minGap = Infinity;
-    
-    array.forEach(el => {
-        const gap = Math.abs(el-n);
-        if(gap < minGap){
-            minGap = gap;
-            result = el;
-        }else if(gap === minGap){
-            result = Math.min(result,el);
-        }
-    })
-    
-    return result;
+    return array.sort((a,b) => Math.abs(a-n) - Math.abs(b-n) || a-b)[0];
 }
