3055b6888268d65397158a96eda674be2f55d59d
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-10 01:42:55 +0900

    [level 2] Title: 짝지어 제거하기, Time: 98.67 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,10 +1,10 @@
 function solution(s){
-    const result = [];
+    const stack = [];
     
     for(const c of s){
-        if(c===result.at(-1)) result.pop();
-        else result.push(c);
+        if(stack.at(-1)===c) stack.pop();
+        else stack.push(c);
     }
     
-    return result.length===0 ? 1 : 0;
+    return stack.length === 0 ? 1 : 0;
 }
