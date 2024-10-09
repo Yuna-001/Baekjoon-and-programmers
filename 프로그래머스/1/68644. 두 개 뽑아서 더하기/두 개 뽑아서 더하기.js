32d999e310f5f2574d4400c84dc41e1fdd39eb9e
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-09 17:39:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,11 +1,11 @@
 function solution(numbers) {
-    const set = new Set();
+    const sums = new Set();
     
-    for(let i=0; i<numbers.length-1; i++){
+    for(let i=0; i<numbers.length; i++){
         for(let j=i+1; j<numbers.length; j++){
-            set.add(numbers[i]+numbers[j]);
+            sums.add(numbers[i]+numbers[j]);
         }
     }
     
-    return [...set].sort((a,b)=>a-b);
+    return [...sums].sort((a,b)=>a-b);
 }
