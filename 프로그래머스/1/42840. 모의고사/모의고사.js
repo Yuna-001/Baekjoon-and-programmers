59f80e0babd9010dd014999ff2385a8f81f5b8bf
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-09 17:57:18 +0900

    [level 1] Title: 모의고사, Time: 2.05 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,21 +1,18 @@
 function solution(answers) {
-    const p1 = [1,2,3,4,5]; 
-    const p2 = [2,1,2,3,2,4,2,5];  
-    const p3 = [3,3,1,1,2,2,4,4,5,5]; 
-    const scores = [0,0,0];
     const result = [];
+    const scores = [];
+    const patterns = [[1,2,3,4,5],[2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]];
+    let max = 0;
     
-    answers.forEach((answer,i)=>{
-        if(p1[i%p1.length] === answer) scores[0]++;
-        if(p2[i%p2.length] === answer) scores[1]++;
-        if(p3[i%p3.length] === answer) scores[2]++;
-    })
-    
-    const max = Math.max(...scores);
+   for(const pattern of patterns){
+       const score = answers.filter((answer,i) => answer === pattern[i%pattern.length]).length;
+       scores.push(score);
+       if(score>max) max = score;
+   }
     
     scores.forEach((score,i)=>{
         if(score === max) result.push(i+1);
-    })  
-  
+    })
+    
     return result;
 }
