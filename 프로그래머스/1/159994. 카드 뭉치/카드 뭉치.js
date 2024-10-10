7794debf94285d7e9313cc5d0b2f63e6ef0a4d07
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-11 00:54:25 +0900

    [level 1] Title: 카드 뭉치, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -2,7 +2,6 @@ function solution(cards1, cards2, goal) {
     let idx1 = 0;
     let idx2 = 0;
     let goalIdx = 0;
-    let isPossible = true;
     
     while(goalIdx < goal.length){
        if(cards1[idx1] === goal[goalIdx]){
@@ -12,10 +11,9 @@ function solution(cards1, cards2, goal) {
            idx2++;
            goalIdx++;
        }else{
-           isPossible =false;
-           break;
+           return "No";
        }
     }
     
-    return isPossible ? "Yes" : "No";
+    return "Yes";
 }
