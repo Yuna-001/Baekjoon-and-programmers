7a50d7354ee990c82a4c434137794ef1985c1348
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-11 17:41:58 +0900

    [level 2] Title: 피로도, Time: 8.41 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,25 @@
+function solution(k, dungeons) {
+    let max = 0;
+    const visited = {};
+    
+    function explore(hp,count){
+        if(count > max){
+            max = count;
+        }
+        
+        for(let i=0; i<dungeons.length; i++){
+            const [minimum,spend] = dungeons[i];
+            if(!visited[i] && hp >= minimum){
+                visited[i] = true;
+                explore(hp-spend,count+1);
+                visited[i] = false;
+            }
+        }
+        
+        return;
+    }
+    
+    explore(k,0);
+    
+    return max;
+}
