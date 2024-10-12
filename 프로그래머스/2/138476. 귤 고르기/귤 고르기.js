3a283be09fb34790aec8231252ced03b52d23bd7
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-13 00:44:24 +0900

    [level 2] Title: 귤 고르기, Time: 199.75 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,8 +1,17 @@
 function solution(k, tangerine) {
+    let type = 0;
     const obj = {};
     
-    tangerine.forEach((size) => obj[size] = (obj[size] || 0) + 1);
-    tangerine.sort((a,b) => obj[b]-obj[a] || a-b);
+    tangerine.forEach(v => {
+        obj[v] = (obj[v] || 0) + 1;
+    })
     
-    return new Set(tangerine.slice(0,k)).size;
+    for(const [size, num] of Object.entries(obj).sort((a,b)=>b[1]-a[1])){
+        type++;
+        k-=num;
+        
+        if(k <= 0) break;
+    }
+    
+    return type;
 }
