24dc0f94fe139654987ad0cf177c6511d02f3ed6
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-13 00:36:33 +0900

    [level 2] Title: 구명보트, Time: 29.10 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,12 +1,16 @@
 function solution(people, limit) {
-    let boat = 0;
+    let count = 0;
+    let left = 0;
+    let right = people.length - 1;
     
     people.sort((a,b)=>a-b);
     
-    for(let i=0,j=people.length-1; i<=j; j--){
-        if(people[i]+people[j]<=limit && i !==j ) i++;
-        boat++;
+    while(left <= right){
+        if(people[left] + people[right] <= limit) left++;
+        
+        right--;
+        count++;
     }
     
-    return boat;
+    return count;
 }
