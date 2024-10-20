5552836b86325f3f87292d896e223e4726e23c68
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-10-20 22:52:58 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,16 +1,15 @@
 function solution(clothes) {
-    const obj = {};
+    const types = {};
+    
+    clothes.forEach(([cloth,type]) => {
+        types[type] = (types[type] || 0) + 1;  
+    })
+    
     let result = 1;
     
-    for (const clothe of clothes){
-        obj[clothe[1]] = ( obj[clothe[1]] || 0 ) + 1;
+    for(const n of Object.values(types)){
+        result *= (n + 1);
     }
     
-    for(const key in obj){
-        // nC1 + (안 입는 경우) 1
-        result *= obj[key] + 1
-    }
-    
-    // 아무것도 안 입는 경우는 빼야하므로 -1
     return result - 1;
 }
