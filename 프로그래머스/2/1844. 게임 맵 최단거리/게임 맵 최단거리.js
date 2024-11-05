4017cce7c787e208631791e94e083ed38df82eef
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-05 22:05:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 36.74 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,23 +1,49 @@
-function solution(maps) {
-    const goal = [maps.length-1, maps[0].length-1];
-    const queue = [[0,0,1]];
-    const directions = [[1,0],[0,1],[-1,0],[0,-1]];
+class Queue{
+    constructor(){
+        this.items = [];
+        this.head = 0;
+        this.rear = 0;
+    }
     
-    while(queue.length){
-        const [row,col,step] = queue.shift();
+    push(val){
+        this.items.push(val);
+        this.rear++;
+    }
+    
+    pop(){
+        return this.items[this.head++];
+    }
+    
+    isEmpty(){
+        return this.head === this.rear;
+    }
+}
+
+function solution(maps) {
+    const queue = new Queue();
+    const m = maps.length;
+    const n = maps[0].length;
+    const visited = Array.from({length:m}, ()=>[]);
+    const directions = [[0,1],[1,0],[0,-1],[-1,0]];
+    
+    queue.push({position: [0,0], distance: 1});
+    
+    while(!queue.isEmpty()){
+        const {position: [y,x], distance} = queue.pop();
         
-        for(const direction of directions){
-            const nextRow = row + direction[0];
-            const nextCol = col + direction[1];
-            
-            if(nextRow === goal[0] && nextCol === goal[1]) return step + 1;
-            
-            if(nextRow >= 0 && nextRow <= goal[0] && nextCol >= 0 && nextCol <= goal[1] && maps[nextRow][nextCol]){
-                queue.push([nextRow,nextCol,step+1]);
-                maps[nextRow][nextCol] = 0;
+        if(y === m-1 && x === n-1) return distance;
+        
+        for(const [dy,dx] of directions){
+            if(isValidPosition(y+dy,x+dx,[m,n],visited,maps)){
+                queue.push({position: [y+dy,x+dx], distance: distance+1});
+                visited[y+dy][x+dx] = true;
             }
         }
     }
     
     return -1;
+}
+
+function isValidPosition(y,x,limit,visited,maps){
+    return  x >= 0 && y >= 0 && x < limit[1] && y < limit[0] && !visited[y][x] && maps[y][x];
 }
