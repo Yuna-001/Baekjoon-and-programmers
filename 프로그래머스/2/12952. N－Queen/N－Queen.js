63abd50d43f01eb71a2d39897fe2ed7e1e9e1c8b
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-08 21:51:41 +0900

    [level 2] Title: N-Queen, Time: 95.79 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,21 +1,8 @@
 function solution(n) {
-    const board = Array.from({length: n}, () => new Array(n).fill(false));
+    const rows = [];
+    const diagonal1 = [];
+    const diagonal2 = [];
     let count = 0;
-    
-    function isValid(row,col){
-        // 같은 열 확인
-        if(board.some(arr => arr[col] === true)) return false;
-        
-        // 대각선 확인
-        for(let i=0; i<n; i++){
-            if(i=== row) continue;
-            
-            const rowGap = Math.abs(row - i);
-            if(board[i][col-rowGap] || board[i][col+rowGap]) return false;
-        }
-        
-        return true;
-    }
     
     function findPosition(row){
         if(row === n){
@@ -24,10 +11,10 @@ function solution(n) {
         }
         
         for(let col=0; col<n; col++){ 
-            if(isValid(row,col)){
-                board[row][col] = true;
+            if(!(rows[col] || diagonal1[col+row] || diagonal2[col+n-row])){
+                rows[col] = diagonal1[col+row] = diagonal2[col+n-row] = true;
                 findPosition(row+1);
-                board[row][col] = false;
+                rows[col] = diagonal1[col+row] = diagonal2[col+n-row] = false;
             }
         }
     }
