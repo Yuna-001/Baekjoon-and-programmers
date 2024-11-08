f2f5c098caea451f5d1915496176fd64a6ae58c2
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-08 21:35:25 +0900

    [level 2] Title: N-Queen, Time: 1115.21 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,54 +1,38 @@
 function solution(n) {
+    const board = Array.from({length: n}, () => new Array(n).fill(false));
     let count = 0;
-    const board = Array.from({length:n},()=>new Array(n).fill(0));
     
-    function isPossible(row,col){
-        // 가로
-        if(board[row].some(v => v === 1)) return false;
+    function isValid(row,col){
+        // 같은 열 확인
+        if(board.some(arr => arr[col] === true)) return false;
         
-        // 세로
+        // 대각선 확인
         for(let i=0; i<n; i++){
-            if(board[i][col] === 1) return false;
-        }
-        
-        // 위쪽 대각선
-        for(let i=0; i<row; i++){
-            const gap = Math.abs(i-row);
+            if(i=== row) continue;
             
-            // 왼쪽 위
-            if(col-gap >= 0 && board[i][col-gap]) return false
-            // 오른쪽 위
-            if(col + gap < n && board[i][col+gap]) return false
-        }
-        
-        //아래쪽 대각선
-        for(let i=row+1; i<n; i++){
-            const gap = Math.abs(i-row);
-            
-            // 왼쪽 아래
-            if(col-gap >= 0 && board[i][col-gap]) return false
-            // 오른쪽 아래
-            if(col + gap < n && board[i][col+gap]) return false
+            const rowGap = Math.abs(row - i);
+            if(board[i][col-rowGap] || board[i][col+rowGap]) return false;
         }
         
         return true;
     }
     
     function findPosition(row){
-        for(let col=0; col<n; col++){
-            if(isPossible(row,col)){
-                if(row === n-1){
-                    count++;
-                }else{
-                    board[row][col] = 1;
-                    findPosition(row+1);
-                    board[row][col] = 0;
-                }
+        if(row === n){
+            count++;
+            return;
+        }
+        
+        for(let col=0; col<n; col++){ 
+            if(isValid(row,col)){
+                board[row][col] = true;
+                findPosition(row+1);
+                board[row][col] = false;
             }
         }
     }
     
-    findPosition(0)
+    findPosition(0);
     
     return count;
 }
