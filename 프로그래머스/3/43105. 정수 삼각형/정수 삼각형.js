8642c87759d1f3be4fff3044074b18e68ef4dc19
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-14 20:51:22 +0900

    [level 3] Title: 정수 삼각형, Time: 5.68 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,19 +1,21 @@
 function solution(triangle) {
-    const length = triangle.length
-    const dp = Array.from({length}, () => new Array());
+    const m = triangle.length;
+    const dp = Array.from({length:m}, (_,i)=> new Array(i+1).fill(0));
+    
     dp[0][0] = triangle[0][0];
     
-    for(let i=1; i<length; i++){
-        for(let j=0; j<triangle[i].length; j++){
-            if(j === 0){
-                dp[i][j] = triangle[i][j] + dp[i-1][j];
-            } else if(j === triangle[i].length -1){
-                dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-            } else{
-                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j], dp[i-1][j-1]);
-            }
+    for(let i=1; i<m; i++){
+        // 첫번째 열
+        dp[i][0] = triangle[i][0] + dp[i-1][0];
+        
+        // 중간 열들
+        for(let j=1; j<triangle[i].length-1; j++){
+            dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1], dp[i-1][j]);
         }
+        
+        // 마지막 열
+        dp[i][i] = triangle[i][i] + dp[i-1][i-1];
     }
     
-    return Math.max(...dp[length-1]);
+    return Math.max(...dp[m-1]);
 }
