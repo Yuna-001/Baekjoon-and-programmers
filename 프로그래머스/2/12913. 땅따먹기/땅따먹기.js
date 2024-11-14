e8f9b8f9c50c159d7ba0464531e785b5f14e3772
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-14 21:28:21 +0900

    [level 2] Title: 땅따먹기, Time: 95.29 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,17 +1,16 @@
 function solution(land) {
-    const m = land.length;
-    const dp = Array.from({length:m},()=>new Array(4));
+    const N = land.length;
+    const dp = Array.from({length:N}, ()=>Array(4).fill(0));
     
-    for(let j=0; j<4; j++){
-        dp[0][j] = land[0][j];
+    for(let i=0; i<4; i++){
+        dp[0][i] = land[0][i];
     }
     
-    for(let i=1; i<m; i++){
-        dp[i][0] = land[i][0] + Math.max(dp[i-1][1],dp[i-1][2],dp[i-1][3]);
-        dp[i][1] = land[i][1] + Math.max(dp[i-1][0],dp[i-1][2],dp[i-1][3]); 
-        dp[i][2] = land[i][2] + Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][3]);
-        dp[i][3] = land[i][3] + Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][2]);
+    for(let i=1; i<N; i++){
+        for(let j=0; j<4; j++){
+            dp[i][j] = land[i][j] + Math.max(...dp[i-1].filter((_,idx) => idx !== j))
+        }
     }
-    
-    return Math.max(...dp[m-1]);
+
+    return Math.max(...dp[N-1]);
 }
