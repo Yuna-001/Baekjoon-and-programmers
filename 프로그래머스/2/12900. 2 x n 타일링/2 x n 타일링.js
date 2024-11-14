48e45d5e4eb8f9a9e4d2c9ade02273810fbf5488
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-11-14 20:18:12 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.97 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,16 +1,9 @@
 function solution(n) {
-    // n을 1과 2의 조합으로 나타낼 수 있는 경우의 수
-    
-    if(n===1) return 1;
-    if(n===2) return 2;
-    
-    let f1 = 1, f2 = 2;
-    let result;
+    const dp = [0,1,2];
     
     for(let i=3; i<=n; i++){
-        result = f1 + f2;
-        [f1,f2] = [f2 % 1000000007, result % 1000000007];
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
     }
     
-    return result % 1000000007;
+    return dp[n];
 }
