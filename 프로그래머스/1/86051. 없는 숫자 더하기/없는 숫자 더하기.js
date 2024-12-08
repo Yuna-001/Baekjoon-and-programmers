14663b8bdbce40e88516cce9a1159d7cbd5e918b
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-12-08 21:59:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,3 @@
 function solution(numbers) {
-    const arr = [0,1,2,3,4,5,6,7,8,9];
-    
-    numbers.forEach(n => {
-        arr[n] = 0;
-    })
-    
-    return arr.reduce((acc,cur)=>acc+cur,0);
+    return numbers.reduce((acc,cur)=> acc-cur, 45);
 }
