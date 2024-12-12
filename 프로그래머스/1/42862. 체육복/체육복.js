c0ad23df6c148e8adb14c07a873f1fa020ff9d96
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-12-12 23:23:31 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,21 +1,22 @@
 function solution(n, lost, reserve) {
-    let count = lost.length;
-    let index = 0;
+    const lostStudents = new Set(lost);
+    const give = [];
     
-    lost.sort((a,b) => a-b);
-    reserve.sort((a,b) => a-b);
-   
-    for(let i=0; i<reserve.length; i++){
-        for(let j=index; j<lost.length; j++){
-            if(Math.abs(reserve[i] - lost[j]) <= 1){
-                if(reserve[i+1]===lost[j]) i++;
-                else if(reserve[i] === lost[j+1]) j++;
-                index = j+1;
-                count--;
-                break;
-            }
+    for(const student of reserve){
+        if(lostStudents.has(student)){
+            lostStudents.delete(student);
+        }else {
+            give.push(student);
         }
     }
     
-    return n - count;
+    give.sort((a,b)=>a-b).forEach(student => {
+        if(lostStudents.has(student-1)){
+            lostStudents.delete(student-1);
+        }else if(lostStudents.has(student+1)){
+            lostStudents.delete(student+1);  
+        }
+    })
+    
+    return n - lostStudents.size;
 }
