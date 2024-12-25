6b7f72224b93c8120bfd82692dcd6336e15696b7
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2024-12-25 20:04:21 +0900

    [level 3] Title: 최고의 집합, Time: 2.09 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,13 +1,12 @@
 function solution(n, s) {
+    if(n > s) return [-1];
+    
     let result = [];
     
     for(let i=0; i<n; i++){
-        const num = parseInt(s/(n-i),10);
-        
-        if(num === 0) return [-1];
-        
-        result[i] = num;
-        s -= num;
+        const element = Math.floor(s/(n-i),10);
+        result[i] = element;
+        s -= element;
     }
     
     return result;
