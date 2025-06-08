376c14f5b7a3d56ba46a4358066ce99ad720080a
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-06-08 16:16:44 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,6 @@
 function solution(a, b, n) {
-    let result = 0;
-    let newCola = 0;
-    
-    while(n >= a){
-        newCola = Math.floor(n/a)*b;
-        result+=newCola;
-        n = n%a + newCola;        
-    }
-    
-    return result;
+    // n-b: 총 교환 가능한 병 수 (아차피 마지막에 교환 받은 b병은 남게 됨)
+    // a-b: 교환할 때마다 줄어드는 병 수
+    // 교환 가능 횟 수: Math.floor((n-b)/(a-b))
+    return Math.floor((n-b)/(a-b))*b;
 }
