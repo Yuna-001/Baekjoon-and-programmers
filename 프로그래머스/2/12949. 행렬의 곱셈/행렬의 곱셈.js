2baa9d59b472cd5b91e1a65da05e72cc39fb9b0a
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-06-15 17:10:04 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.77 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,13 +1,11 @@
 function solution(arr1, arr2) {
-    // 3*7 x 7*2 => 3*2
-    const result = Array.from({length:arr1.length},()=> new Array(arr2[0].length));
+    const result = Array.from({length: arr1.length},()=>new Array(arr2[0].length).fill(0));
     
     for(let i=0; i<arr1.length; i++){
-        for(let j=0,sum=0; j<arr2[0].length; j++,sum=0){
-            for(let k=0; k<arr1[0].length; k++){
-                sum += arr1[i][k]*arr2[k][j];
-            }
-            result[i][j] = sum;
+        for(let j=0; j<arr2[0].length; j++){
+          for(let k=0; k<arr2.length; k++){
+              result[i][j] += arr1[i][k]*arr2[k][j];
+          }
         }
     }
     
