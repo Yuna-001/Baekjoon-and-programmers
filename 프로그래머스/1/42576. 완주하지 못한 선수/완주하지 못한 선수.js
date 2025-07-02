41f649fc2c5b3093fe982f004093101981cb6141
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-02 23:06:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.80 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,17 @@
 function solution(participant, completion) {
     const players = {};
     
-    participant.forEach(p=>{
-        if(players[p]) players[p]++;
-        else players[p] = 1;
-    })
+    for(const player of participant){
+        players[player] = (players[player] || 0) + 1;
+    }
     
-    completion.forEach(p=>{
-        if(players[p] === 1) delete players[p];
-        else players[p]--;
-    })
+    for(const player of completion){
+        if(players[player] === 1){
+            delete players[player];
+        }else{
+            players[player] -= 1;
+        }
+    }
     
     return Object.keys(players)[0];
 }
