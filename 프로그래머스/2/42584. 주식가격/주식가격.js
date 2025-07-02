a448eeadc87f9f54bccb69ed0d7be9fe81c50290
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-02 15:01:17 +0900

    [level 2] Title: 주식가격, Time: 17.28 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,20 +1,20 @@
 function solution(prices) {
-    const result = Array(prices).fill(0);
+    const result = [];
     const stack = [];
     
     for(let i=0; i<prices.length; i++){
-        while(stack.length > 0 && stack.at(-1).price > prices[i]){
-            const { idx } = stack.pop();
-            result[idx] = i - idx;
+        while(stack.length > 0 && prices[stack.at(-1)] > prices[i]){
+            const idx = stack.pop();
+            result[idx] = i-idx;
         }
-            
-         stack.push({idx : i, price : prices[i]});
+        
+        stack.push(i);
     }
     
     while(stack.length > 0){
-        const { idx } = stack.pop();
-        result[idx] = prices.length - idx - 1;;
+        const idx = stack.pop();
+        result[idx] = prices.length-1-idx;
     }
-    
+   
     return result;
 }
