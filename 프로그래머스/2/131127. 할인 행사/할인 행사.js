d1026330f89a52e12ccb3dc2531ae722fdd08aea
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-02 23:40:49 +0900

    [level 2] Title: 할인 행사, Time: 143.10 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,20 +1,29 @@
 function solution(want, number, discount) {
-    const target = {};
-    let count = 0;
+    let result = 0;
+    const items = {};
     
-    want.forEach((item,i) => {
-        target[item] = number[i];
-    })
-   
-    for(let i=0; i<discount.length; i++){
-        const copy = new Map(Object.entries(target));
-        for(let j=0; j<10; j++){
-            const n = copy.get(discount[i+j]);
-            if(n === 1) copy.delete(discount[i+j]);
-            else if (n > 1) copy.set(discount[i+j], n-1);
-        }
-        if(copy.size === 0) count++;
+    for(let i=0; i<want.length; i++){
+        items[want[i]] = number[i];
     }
     
-    return count;
+    for(let start=0; start<=discount.length-10; start++){
+        const copy = new Map(Object.entries(items));
+        
+        for(let i=0; i<10; i++){
+            const item = discount[start+i];
+            
+            if(copy.has(item)){
+                const n = copy.get(item);
+                if(n === 1){
+                    copy.delete(item)
+                }else{
+                    copy.set(item, n-1)
+                }
+            }
+        }
+        
+        if(copy.size === 0) result++;
+    }
+    
+    return result;
 }
