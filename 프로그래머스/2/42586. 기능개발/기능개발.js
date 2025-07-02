6a396f145e2292a7303860cc153c53a224fa838b
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-02 16:51:58 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,23 +1,25 @@
 function solution(progresses, speeds) {
-    if(progresses.length === 0) return [];
-    
     const counts = [];
-    let count = 1;
-    let maxDay = Math.ceil((100-progresses[0])/speeds[0]);
+    const days = [];
     
-    for(let i=1; i<progresses.length; i++){
-        const day = Math.ceil((100-progresses[i])/speeds[i]);
-        
-        if(day <= maxDay){
+    for(let i=0; i<progresses.length; i++){
+        days.push(Math.ceil((100-progresses[i])/speeds[i]));
+    }
+    
+    let count = 0;
+    let maxDay = days[0];
+    
+    for(let i=0; i<days.length; i++){
+        if(days[i] <= maxDay){
             count++;
         }else{
-            counts.push(count);
-            maxDay = day
+            counts.push(count)
+            maxDay = days[i];
             count = 1;
-        } 
+        }
     }
     
     counts.push(count);
-
+    
     return counts;
 }
