6a20a1d44134c29fcb17ca30784d763a2c2bb327
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-05 21:12:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 247.47 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,22 +1,33 @@
 function solution(id_list, report, k) {
-    const reportedUsers = {};
-    
-    for(const arr of report){
-        const [reporting,reported] = arr.split(" ");
-        
-        if(!reportedUsers[reported]) reportedUsers[reported] = new Set();
-        reportedUsers[reported].add(reporting);
-    }
-    
+    // 내가 신고한 사람이 정지되었는지
+    // 그게 몇 번인지
+    const user = {};
+    const reportedUser = {};
     const mail = {};
     
-    for(const reported in reportedUsers){
-        if(reportedUsers[reported].size < k) continue;
-            
-        for(const reporting of reportedUsers[reported]){
-            mail[reporting] = (mail[reporting] || 0) + 1;
+    for(const str of report){
+        const [reporting, reported] = str.split(" ");
+        
+        if(!reportedUser[reported]){
+            reportedUser[reported] = new Set();
+        }
+        
+        reportedUser[reported].add(reporting);
+    }
+    
+    for(const reported in reportedUser){
+        if(reportedUser[reported].size >= k){
+            for(const reporting of reportedUser[reported]){
+                mail[reporting] = (mail[reporting] || 0) + 1;
+            }
         }
     }
     
-    return id_list.map(id => mail[id] ? mail[id] : 0);
+    const result = [];
+    
+    for(const id of id_list){
+        result.push(mail[id] ?? 0);
+    }
+    
+    return result;
 }
