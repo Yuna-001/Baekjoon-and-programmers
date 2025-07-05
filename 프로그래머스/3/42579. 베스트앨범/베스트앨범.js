01f10b59abbaa2877e280766f5da533fae053aab
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-07-05 15:15:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,36 +1,29 @@
 function solution(genres, plays) {
-    const result = [];
+    const genrePopularity = {};
+    const music = {};
     
-    // 장르별 총 플레이 수 계산, genrePlays에 저장
-    const genrePlays = {};
-    
-    genres.forEach((genre,i)=>{
-        genrePlays[genre] = (genrePlays[genre] || 0) + plays[i];
-    })
-    
-    // 장르 우선 순위, genreOrder에 저장
-    const genreOrder = Object.entries(genrePlays).sort((a,b)=>b[1]-a[1]).map(([genre])=>genre);
-    
-    // 장르별로 곡 정리, genreList에 저장
-    const genreList = {};
-    
-    plays.forEach((play,i)=>{
-        if(!genreList[genres[i]]) genreList[genres[i]] = [];
-        genreList[genres[i]].push([play,i]);
-    })
-    
-    // genreList에서 장르별로 플레이수에 따라 내림차순 정렬
-    for(const genre in genreList){
-        genreList[genre].sort((a,b)=>b[0]-a[0]);
+    for(let i=0; i<genres.length; i++){
+        const genre = genres[i]
+        genrePopularity[genre] = (genrePopularity[genre] || 0 ) + plays[i];
+        
+        if(!music[genre]) music[genre] = [];
+        music[genre].push({index:i,play:plays[i]});
     }
     
-    // 장르별로 2개씩 result에 저장
-    genreOrder.forEach((genre,i)=>{
-        const min = Math.min(2, genreList[genre].length);
-        for(let i=0; i<min; i++){
-            result.push(genreList[genre][i][1]);
+    const genreRanking = Object.entries(genrePopularity).sort((a,b)=>b[1]-a[1]);
+    
+    const result = [];
+    
+    for(const [genre] of genreRanking){
+        const genreMusicRanking = music[genre].sort((a,b)=>{
+            if(a.play === b.play) return a.index-b.index;
+            return b.play-a.play;
+        });
+        
+        for(let i=0; i<Math.min(2,genreMusicRanking.length); i++){
+            result.push(genreMusicRanking[i].index);
         }
-    })
-
+    }
+    
     return result;
 }
