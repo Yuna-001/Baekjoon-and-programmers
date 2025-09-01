bc9a51b5d0174f445d91590ed36713123844cd8d
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-09-01 16:43:02 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 34.35 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 49.2 MB, 시간: 38.81 ms
+메모리: 49 MB, 시간: 34.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 23:05:54
+2025년 09월 01일 16:43:01
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,36 +1,25 @@
 function solution(enroll, referral, seller, amount) {
-    const tree = {};
+    const PRICE = 100;
+    const enrollMap = {};
+    const incomeMap = {}
     
-    enroll.forEach((p,i)=>{
-        tree[p] = {};
-        tree[p].referral = referral[i];
-        tree[p].earning = 0;
-    })
+    for(let i=0; i<enroll.length; i++){
+        enrollMap[enroll[i]] = referral[i];
+        incomeMap[enroll[i]] = 0;
+    }
     
     for(let i=0; i<seller.length; i++){
-        let current = tree[seller[i]];
-        let earning = amount[i] * 100;
-        // 추천인이 "-"인 경우 소득은 주고 stop
-        // 10%가 1원 미만인 경우 그냥 stop
-        while(current){
-            const share = Math.floor(earning * 0.1);
-            
-            if(share < 1){
-                current.earning += earning;
-                break;
-            }
-            
-            current.earning += earning - share;
-            earning = share;
-            current = tree[current.referral];
+        let total = amount[i]*PRICE;
+        let person = seller[i];
+        
+        while(person && person !== "-" && total > 0){
+            const giving = Math.floor(total*0.1);
+            incomeMap[person] += total - giving;
+            total = giving;
+            person = enrollMap[person];
         }
     }
     
-    const result = [];
+    return enroll.map(person => incomeMap[person]);
     
-     enroll.forEach((p,i)=>{
-         result.push(tree[p].earning);
-     })
-    
-    return result;
 }
