2490b999c540db42587021dd32c1ec02a83745f2
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-09-02 22:23:50 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,12 +1,16 @@
 function solution(n, words) {
-    const set = new Set([words[0]]);
+    const used = new Set();
+    used.add(words[0]);
     
     for(let i=1; i<words.length; i++){
-        if(words[i][0] !== words[i-1].at(-1) || set.has(words[i])){
-            return [i%n+1, Math.floor(i/n)+1];
+        const prev = words[i-1];
+        const cur = words[i];
+        
+        if(prev.at(-1) !== cur[0] || used.has(cur)){
+            return [i%n+1, Math.floor(i/n)+1]
         }
         
-        set.add(words[i]);
+        used.add(cur);
     }
     
     return [0,0];
