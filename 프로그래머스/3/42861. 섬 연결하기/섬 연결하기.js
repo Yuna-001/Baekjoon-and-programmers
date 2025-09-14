e7cf08f9ef500efaf5d013c7df5424bf22e24221
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-09-14 22:12:13 +0900

    [level 3] Title: 섬 연결하기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,26 +1,40 @@
+function find(parent,node){
+    if(parent[node] === node) return node;
+    parent[node] = find(parent,parent[node]);
+    return parent[node];
+}
+
+function union(root1, root2, parent, rank){
+    if(rank[root1] < rank[root2]){
+        parent[root1] = root2;
+    }else if(rank[root2] < rank[root1]){
+        parent[root2] = root1;
+    }else{
+        parent[root1] = root2;
+        rank[root2]++;
+    }
+}
+
 function solution(n, costs) {
-    const roots = Array.from({length:n},(_,i)=>i);
+    const parent = Array.from({length: n},(_,i)=>i);
+    const rank = new Array(n).fill(0);
+    let minCost = 0;
+    let edges = 0;
+    
     costs.sort((a,b)=>a[2]-b[2]);
-    
-    let result = 0;
-    
-    for(let [v1,v2,cost] of costs){
-        const root1 = find(roots,v1);
-        const root2 = find(roots,v2);
+  
+    for(const [land1, land2, cost] of costs){
+        if(edges === n-1) break;
+        
+        const root1 = find(parent,land1);
+        const root2 = find(parent,land2);
         
         if(root1 === root2) continue;
         
-        result += cost;
-        roots[root2] = root1;
+        union(root1,root2,parent,rank);
+        minCost += cost;
+        edges++;
     }
     
-    return result;
-}
-
-function find(parents,x){
-    if(parents[x] === x) return x;
-    
-    parents[x] = find(parents,parents[x]);
-    
-    return parents[x];
+    return minCost;
 }
