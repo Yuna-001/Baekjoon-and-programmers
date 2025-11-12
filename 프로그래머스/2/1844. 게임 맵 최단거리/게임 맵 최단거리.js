9f626c16e1faeeaacf369e548f62a737c4caea9a
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-11-12 22:12:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 39.11 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,51 +1,23 @@
-class Queue{
-    constructor(){
-        this.items = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    
-    isEmpty(){
-        return this.front === this.rear;
-    }
-    
-    push(item){
-        this.items.push(item);
-        this.rear++;
-    }
-    
-    pop(){
-        return this.items[this.front++];
-    }
-}
-
 function solution(maps) {
-    const goal = [maps[0].length-1, maps.length-1];
-    
-    const dirs = [[0,1],[1,0],[0,-1],[-1,0]];
-    
-    const queue = new Queue();
-    queue.push([0,0,1]);
-    
-    while(!queue.isEmpty()){
-        const [x,y,step] = queue.pop();
-       
-        if(x === goal[0] && y === goal[1]) return step;
-        
-        for(const [dx,dy] of dirs){
-            const nextX = x + dx;
-            const nextY = y + dy;
-            
-            if(isValidPosition(nextX,nextY,maps)){
-                queue.push([nextX, nextY, step+1])
-                maps[nextY][nextX] = 0;;
+    const goal = [maps.length-1, maps[0].length-1];
+    const queue = [[0,0,1]];
+    const directions = [[1,0],[0,1],[-1,0],[0,-1]];
+
+    while(queue.length){
+        const [row,col,step] = queue.shift();
+
+        if(row === goal[0] && col === goal[1]) return step;
+
+        for(const direction of directions){
+            const nextRow = row + direction[0];
+            const nextCol = col + direction[1];
+
+            if(nextRow >= 0 && nextRow <= goal[0] && nextCol >= 0 && nextCol <= goal[1] && maps[nextRow][nextCol]){
+                queue.push([nextRow,nextCol,step+1]);
+                maps[nextRow][nextCol] = 0;
             }
         }
     }
-    
-    return -1;
-}
 
-function isValidPosition(x,y,maps){
-    return y >= 0 && x >= 0 && y < maps.length && x < maps[0].length && maps[y][x]===1;
+    return -1;
 }
