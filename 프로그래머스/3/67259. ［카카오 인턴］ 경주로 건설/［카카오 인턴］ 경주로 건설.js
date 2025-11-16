93b13333cb76f777574a957506adbb25760e6b91
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-11-16 18:35:30 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 6810.72 ms, Memory: 1.13 GB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -3,35 +3,35 @@ function solution(board) {
     const M = board.length;
     const N = board[0].length;
     const costs = Array.from({length: M},()=> Array.from({length: N}, () => new Array(4).fill(Infinity)));
-    
+
     const queue = new Queue();
-    
+
     queue.push([0,0,-1,0]);
     costs[0][0] = [0,0,0,0];
-    
+
     while(!queue.isEmpty()){
         const [y,x,prevDirection,cost] = queue.pop();
-        
+
         for(let i=0; i<4; i++){
             const [dy,dx] = directions[i];
             const nextX = x + dx;
             const nextY = y + dy;
-            
+
             if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M || board[nextY][nextX]) continue;
-            
+
             let newCost = cost + 100;
-            
+
             if(prevDirection !== -1 && prevDirection !== i) {
                 newCost += 500;
             }
-            
+
             if(newCost <= costs[nextY][nextX][i]){
                 costs[nextY][nextX][i] = newCost;
                 queue.push([nextY,nextX,i,newCost]);
             }
         }
     }
-    
+
     return Math.min(...costs[M-1][N-1]);
 }
 
@@ -41,16 +41,16 @@ class Queue{
         this.front = 0;
         this.rear = 0;
     }
-    
+
     isEmpty(){
         return this.front === this.rear;
     }
-    
+
     push(val){
         this.items.push(val);
         this.rear++;
     }
-    
+
     pop(){
         return this.items[this.front++];
     }
