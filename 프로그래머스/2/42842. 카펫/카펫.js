d0e4120d071752c85ee43f51cb830f1585f6e8b2
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-11-17 20:37:59 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,9 +1,13 @@
 function solution(brown, yellow) {
-    const total = brown + yellow;
-    for(let rows=3; rows<=Math.sqrt(brown+yellow); rows++){
-        if(total%rows===0){
-            const cols = total/rows;
-            if((cols-2)*(rows-2) === yellow) return [cols, rows];
-        }
+    // brown = (가로+세로)*2-4
+    // brown + yello = 가로*세로
+    
+    const total = brown+yellow;
+    
+    for(let row=3; row<total; row++){
+        if(total%row !==0) continue;
+        
+        const col = total/row;
+        if((row+col)===(brown+4)/2) return [col,row];
     }
 }
