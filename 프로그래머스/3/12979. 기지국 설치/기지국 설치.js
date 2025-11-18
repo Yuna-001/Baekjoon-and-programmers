6981f713734ca7a6996d28b290f66df4a0a6843d
Lee Yuna <161466246+Yuna-001@users.noreply.github.com>
2025-11-18 21:03:19 +0900

    [level 3] Title: 기지국 설치, Time: 2.35 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -2,27 +2,16 @@ function solution(n, stations, w) {
     let count = 0;
     const range = w * 2 + 1;
     
-    // 첫번째 기지국의 전파가 안 닿는 곳 범위
-    const firstGap = stations[0] - w - 1
+    let startIndex = 0;
     
-    if(firstGap >= 1){
-        count += Math.ceil(firstGap/range);
+    for(let i=0; i<stations.length; i++){
+        const endIndex = stations[i] - w - 1;
+        
+        count += Math.ceil((endIndex - startIndex)/range);
+        startIndex = stations[i] + w;
     }
-    
-    
-    for(let i=1; i<stations.length; i++){
-        const cur = stations[i];
-        const prev = stations[i-1];
-        const gap = cur-prev-range;
-        count += Math.ceil(gap/range);
-    }
-    
-    const lastGap = n - (stations.at(-1) + w);
-    
-    if(lastGap >= 1){
-        count += Math.ceil(lastGap/range);
-    }
-    
+
+    count += Math.ceil((n-startIndex)/range);
 
     return count;
 }
